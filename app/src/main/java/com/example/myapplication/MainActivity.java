30885f6060a93f149a5d63fb536660c0c40f6a85
Odamegithub <odamesteve@gmail.com>
2023-07-02 01:04:43 +0000

    pushing to github

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import static com.example.myapplication.R.*;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    }
+
